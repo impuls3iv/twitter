027df3897ff80cafd33a7f57670133d19e8c9b49
Impuls3iv <sammy.unterman@gmail.com>
2016-07-08 21:30:55 -0400

    added part of ajax function

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -1,2 +1,22 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function(){
+
+  //grab the click event
+  $('#tweet-message').on('click', function(e){
+    e.preventDefault();
+  //start ajax function
+    $.ajax({
+      url: $(this).attr('action'),
+      method: "POST"
+      data: $(this).serialize
+      dataType: "POST"
+    }).done(function(responseData){
+      
+    }.always{
+
+    }.fail{
+
+    });//end of ajax
+  });// end of click event
+}); //end of on load function
