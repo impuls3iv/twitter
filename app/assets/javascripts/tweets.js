b8128e02c7e3111ec1ce6dcb0ebe3f3b5a31c2f8
Impuls3iv <sammy.unterman@gmail.com>
2016-07-10 21:13:57 -0400

    still working on 400 error, awaiting stackoverflow response

--- app/assets/javascripts/tweets.js
+++ app/assets/javascripts/tweets.js
@@ -4,23 +4,24 @@ $(function(){
 
   //grab the click event
   $('#new_tweet').on('submit', function(e){
+    var valuesToSubmit = $(this).serialize;
     console.log('point one')
     //var self = $(this)
     e.preventDefault();
+    $('.tweets').html('');
 
     //start ajax function
     $.ajax({
-      url:$(this).attr('action'),
+      url: $(this).attr('action'),
       method: 'POST',
-      data: $(this).serialize,
+      data: valuesToSubmit,
       dataType: 'html'
-     }).done(function(responseData){
-      //var item = ('<li class = "tweet"><p></p></li>');
-      //post tweet to the list
-
-      $('.tweet').html("");
-      $('<li>' + responseData + '</li>').appendTo('.tweet');
-
-    });//end of ajax
+    });
+    //  }).done(function(responseData){
+    //   //var item = ('<li class = "tweet"><p></p></li>');
+    //   //post tweet to the list
+    //   $('<li>' + responseData + '</li>').appendTo('.tweets');
+    //
+    // });//end of ajax
   });// end of click event
 }); //end of on load function
